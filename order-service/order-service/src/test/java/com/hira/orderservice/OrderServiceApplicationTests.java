d3872348783bdb824a4de7fe6e4f713156ae4e1f
hirantha-pabasara <hiranthapabasara@yahoo.com>
2026-02-19 11:44:44 +0530

    first commit

---
+++ order-service/order-service/src/test/java/com/hira/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hira.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
