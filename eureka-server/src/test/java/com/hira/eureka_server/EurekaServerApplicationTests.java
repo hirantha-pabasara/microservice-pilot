3f72df40a1886c873ee43c71a085a807c80efdd9
hirantha-pabasara <hiranthapabasara@yahoo.com>
2026-02-19 11:47:08 +0530

    secound commit

---
+++ eureka-server/src/test/java/com/hira/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hira.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ product-service/src/test/java/com/hira/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hira.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
